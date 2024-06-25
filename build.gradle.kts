7ee9f94c03396c7b6a6636eba366bfdef6e44ca8
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2024-06-25 16:54:59 +0200

    Update project structure & update to kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,64 @@
-val publicationName = "kotlin-obd-api"
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    alias(libs.plugins.jvm)
     `maven-publish`
 }
 
 group = "com.github.eltonvs"
-version = "1.3.0"
+version = "1.4.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
-    testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.kotlin.test)
 }
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("kotlin-obd-api") {
             groupId = project.group.toString()
             artifactId = project.name
             version = project.version.toString()
 
             from(components["java"])
+
+            pom {
+                name = "Kotlin OBD API"
+                description = "A lightweight and developer-driven API to query and parse OBD commands"
+                url = "https://github.com/eltonvs/kotlin-obd-api"
+
+                licenses {
+                    license {
+                        name = "Apache License v2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "eltonvs"
+                        name = "Elton Viana"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/eltonvs/kotlin-obd-api.git"
+                    developerConnection = "scm:git:git@github.com:eltonvs/kotlin-obd-api.git"
+                    url = "https://github.com/eltonvs/kotlin-obd-api"
+                }
+            }
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
 rootProject.name = "kotlin-obd-api"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
